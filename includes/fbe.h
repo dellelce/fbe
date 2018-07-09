d6c4245cbce49e99f6669480e89722645469cec9
Antonio Dell'Elce <antouk@dellelce.com>
2018-07-09 17:49:11 +0100

    summary structure

--- includes/fbe.h
+++ includes/fbe.h
@@ -22,8 +22,28 @@ typedef struct _fbe_stats
 {
  unsigned int        total;
  unsigned int        available;
- fbe_stat_entry_ti  *entries;
+ fbe_stat_entry_t   *entries;
 } fbe_stats_t;
+
+// summaries
+
+typedef struct _fbe_stats_summary
+{
+ int            charCount[256];
+
+ // keep track of upper 4 bits: TODO: check endianity issues at byte level
+ // zone is defined as "upper nibble" (or first 4 bits)
+ unsigned short zones[16];
+ unsigned short zone;
+
+ // lowers
+ unsigned short lowers[16];
+ unsigned short lower;
+
+ // total bits
+ unsigned long total_bits;
+} fbe_stats_summary_t;
+
 
 
 #endif // __FBE_H
