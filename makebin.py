a94f9b4a46433c679da8fbfb6a87b79c99d08229
Antonio Dell'Elce <antouk@dellelce.com>
2020-01-27 20:25:29 +0000

    restyle

--- makebin.py
+++ makebin.py
@@ -7,66 +7,65 @@
 
 
 class binspec(object):
-  """
-  Use a kind of RPN language with two stacks (data, commands) and two registers (state, accumulator)
-  To  generate a binary file
+    """
+    Use a kind of RPN language with two stacks (data, commands) and two registers (state, accumulator)
+    To  generate a binary file
 
-  *WIP*
+    *WIP*
 
-  Commands:
+    Commands:
 
-   pop    remove top from data stack
-   xor    xor top from stack with accumulator
-   or
-   and
-   n:      nibble commands TBD
-   n:copy
-   n:copywithstep
-   n:range
-   add
-   minus
-   reverse
+     pop    remove top from data stack
+     xor    xor top from stack with accumulator
+     or
+     and
+     n:      nibble commands TBD
+     n:copy
+     n:copywithstep
+     n:range
+     add
+     minus
+     reverse
 
-
-  Data types:
+   Data types:
 
    * strings (single or double quotes we don't care)
    * hex objects (0x....)
    * integer (anything else starting with 1-9 that works with python int()
-  """
+   """
+    def __init__(self, name):
+        """Read specification from file."""
+        self.stack = []
+        self.bytes = 0
 
-  def __init__(self, name):
-    """Read specification from file."""
-    self.stack = []
-    self.bytes = 0
+        with open(name) as f:
+            _lines = f.readlines()
 
-    with open(name) as f:
-      _lines = f.readlines()
+            # read line ignoring comments - crudely for now
+            self.lines = \
+              [line for line in _lines
+               if line.lstrip().split()[0][0] != '#']
 
-      # read line ignoring comments - crudely for now
-      self.lines = \
-        [line for line in _lines
-         if line.lstrip().split()[0][0] != '#']
+    def __repr__(self):
+        """Use number of lines only, for now at least."""
+        return 'binspec({} lines)'.format(len(self.lines))
 
-  def __repr__(self):
-    """Use number of lines only, for now at least."""
-    return 'binspec({} lines)'.format(len(self.lines))
+    def getbyte(self):
+        """Get byte from buffer of if exhausted re-generate following instructions"""
+        pass
 
-  def getbyte(self):
-    """Get byte from buffer of if exhausted re-generate following instructions"""
-    pass
+    def write(self, name, nbytes):
+        """Write nbytes to file using current specification."""
+        pass
 
-  def write(self, name, nbytes):
-    """Write nbytes to file using current specification."""
-    pass
-
-  def _process(self):
-    """Process configuration in some sort of bytecode"""
+    def _process(self):
+        """Process configuration in some sort of bytecode"""
+        pass
 
 
 if __name__ == '__main__':
-  # test only here
-  bs = binspec('test.binspec')
-  print(bs)
+    # test only here
+    bs = binspec('test.binspec')
+    print(bs)
 
 ## EOF ##
